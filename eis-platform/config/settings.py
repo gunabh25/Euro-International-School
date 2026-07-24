fd39feaaebd8ee39bc275b649aca04918e8ddf20
Gunabh Sharan <gunabhsharan.25@gmail.com>
2026-07-24 22:39:57 +0530

    update TIME_ZONE to 'Asia/Kolkata' and configure REST framework default renderer

--- eis-platform/config/settings.py
+++ eis-platform/config/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+    ]
+}
