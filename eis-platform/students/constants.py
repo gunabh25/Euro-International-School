efd61a53cadfad00d678789ba9b93f6af338f27f
Gunabh Sharan <gunabhsharan.25@gmail.com>
2026-07-24 22:45:04 +0530

    add constants for subjects, date formats, and CSV column mappings

---
+++ eis-platform/students/constants.py
@@ -0,0 +1,23 @@
+SUBJECTS = [
+    "Maths",
+    "Science",
+    "English",
+    "Hindi",
+    "Social Science",
+]
+
+DATE_FORMATS = [
+    "%Y-%m-%d",
+    "%d/%m/%Y",
+    "%d-%b-%Y",
+]
+
+CSV_COLUMNS = {
+    "admission_no": "admission_no",
+    "name": "student_name",
+    "class": "class",
+    "section": "section",
+    "dob": "date_of_birth",
+    "subject": "subject",
+    "marks": "marks_obtained",
+}
