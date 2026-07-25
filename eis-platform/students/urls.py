51cd3b72732fffda92a6fed1d00d0595864561d3
Gunabh Sharan <gunabhsharan.25@gmail.com>
2026-07-25 09:22:19 +0530

    Refactor URL patterns for student-related views and clean up comments

--- eis-platform/students/urls.py
+++ eis-platform/students/urls.py
@@ -8,31 +8,24 @@ from .views import (
 )
 
 urlpatterns = [
-    # Student List
     path(
         "students/",
         StudentListAPIView.as_view(),
         name="student-list",
     ),
-
-    # Student Details
     path(
         "students/<str:admission_no>/",
         StudentDetailAPIView.as_view(),
         name="student-detail",
     ),
-
-    # Summary
     path(
         "summary/",
         SummaryAPIView.as_view(),
         name="summary",
     ),
-
-    # Apply Corrections
     path(
         "marks/corrections/",
         CorrectionAPIView.as_view(),
-        name="mark-correction",
+        name="correction",
     ),
 ]
